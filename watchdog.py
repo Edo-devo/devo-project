ce3b1e0981e6d2f4a6204524bf5f15c4b69c5ea8
Eduard Kushchyan <Eduardo008@yandex.com>
2026-04-01 01:26:49 +0300

    fixbag0.1

--- watchdog.py
+++ watchdog.py
@@ -2,21 +2,30 @@ import requests, time, subprocess
 
 URL = "http://localhost"
 LOG_FILE = "/home/admin/watchdog.log"
+
 TOKEN = "8720397267:AAFqBU_vahEpBR1Qm4TDvwdFpr-d_xdRak0"
 CHAT_ID = "8720397267"
 
 
 def send_alert(msg):
-    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-                  data={"chat_id": CHAT_ID, "text": msg})
+    r = requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": msg}
+    )
+    print("TG:", r.status_code, r.text)
+
 while True:
     try:
         r = requests.get(URL, timeout=5)
         if r.status_code != 200:
             raise Exception(f"Bad status code: {r.status_code}")
-    except requests.exceptions.RequestException as e:
+
+    except Exception as e:
         with open(LOG_FILE, "a") as f:
-            f.write(f"{time.ctime()} -ERROR: {e}\n")
-            subprocess.run(["sudo", "systemctl", "restart", "nginx"])
-            send_alert(f"Сайт упал -> перезапущен\n Ошибка: {e}")
+            f.write(f"{time.ctime()} - ERROR: {e}\n")
+
+        subprocess.run(["systemctl", "restart", "nginx"])
+
+        send_alert(f"Сайт упал → перезапущен\nОшибка: {e}")
+
     time.sleep(60)
